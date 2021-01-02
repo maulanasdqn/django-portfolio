fbff5e5a0d46119f1b63768d70b76a5ac74d0f7f
mrzte <sodiqincahyana1@gmail.com>
2021-01-02 19:39:12 +0700

    Add Postgresql Database

--- Personal/settings.py
+++ Personal/settings.py
@@ -74,13 +74,23 @@ WSGI_APPLICATION = 'Personal.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'contoh', 
+        'USER': 'admin', 
+        'PASSWORD': 'admin',
+        'HOST': '127.0.0.1', 
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
