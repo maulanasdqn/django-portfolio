f2d8466569d6bbeb2a33e4863636acec50753b03
mrzte <sodiqincahyana1@gmail.com>
2021-01-03 11:03:15 +0700

    Add Admin Panel

--- Portfolio/models.py
+++ Portfolio/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Data(models.Model):
+    header = models.CharField(max_length=20)
+    judul = models.CharField(max_length=200)
+    tentang = models.TextField()
+
+    def __str__(self):
+        return self.judul
--- Portfolio/views.py
+++ Portfolio/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
+from .models import Data
 
 def index(request):
-    return render(request, 'Frontend/index.html')
+    data = Data.objects.all()
+    g = {'data':data}
+    return render(request, 'Frontend/index.html',g)
+
+def panel(request):
+    return render(request, 'Backend/index.html')
